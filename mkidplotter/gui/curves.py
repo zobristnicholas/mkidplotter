f39af5e6d7e882c7cd99a14028f024424be81810
SKYNET\mazinlab <nzobrist@physics.ucsb.edu>
2021-05-06 12:05:42 -0700

    change to dBc/Hz units in the noise plot

--- mkidplotter/gui/curves.py
+++ mkidplotter/gui/curves.py
@@ -59,9 +59,7 @@ class NoiseResultsCurve(MKIDResultsCurve):
             dx = x_data[1] - x_data[0]
             x_data.append(x_data[-1] + dx)
             x_data = [x - dx / 2 for x in x_data]
-            # pyqtgraph version 0.12.1 log scaling in y is broken so we do it manually here
-            # see also widgets.NoisePlotWidget, and y axis units in the config file
-            self.setData(x_data, np.log10(y_data), stepMode=True)
+            self.setData(x_data, 10 * np.log10(y_data), stepMode=True)
 
 
 class HistogramResultsCurve(MKIDResultsCurve):
