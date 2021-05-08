3830b61864557427b8671edd305e9f6f3dfc7ed8
SKYNET\mazinlab <nzobrist@physics.ucsb.edu>
2021-05-08 13:17:49 -0700

    no longer having issues with numpy warnings

--- mkidplotter/__init__.py
+++ mkidplotter/__init__.py
@@ -13,18 +13,3 @@ from mkidplotter.icons.manage_icons import get_image_icon
 # bring important pymeasure classes and functions to the top level
 from pymeasure.experiment import IntegerParameter, FloatParameter, VectorParameter
 
-# TODO: suppress problematic warnings locally using with statement
-# get rid of numpy deprecation warnings
-import warnings
-import numpy as np
-warning = "using a non-integer number instead of an integer will result in an error " + \
-          "in the future"
-# warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning, message=warning)
-# warnings.filterwarnings("ignore", message="All-NaN slice encountered",
-#                         category=RuntimeWarning, module="numpy")
-# warnings.filterwarnings("ignore", message="All-NaN axis encountered",
-#                         category=RuntimeWarning, module="numpy")
-# warnings.filterwarnings("ignore", message="All-NaN slice encountered",
-#                         category=RuntimeWarning, module="pyqtgraph")
-# warnings.filterwarnings("ignore", message="invalid value encountered in",
-#                         category=RuntimeWarning, module="pyqtgraph")
